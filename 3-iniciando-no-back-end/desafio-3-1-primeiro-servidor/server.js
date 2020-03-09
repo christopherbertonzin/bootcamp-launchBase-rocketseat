309e42f8726fb0bbdfa84317f4e3d2fd94c66e49
Christopher Moreto Bertonzin <>
2020-03-09 07:46:01 -0300

    Subindo arquivos

---
+++ 3-iniciando-no-back-end/desafio-3-1-primeiro-servidor/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const server = express()
+const port = 3000
+
+// STARTING SERVER
+server.listen(port, () => console.log(`Example server listening on port port!`))
+
+server.use(express.static('public'))
+
+// NUNJUCKS
+server.set("view engine", "njk")
+
+nunjucks.configure('views', {
+    express: server
+})
+
+// ROUTES
+server.get('/', (req, res) => res.render('layout'))
+server.get('/about', (req, res) => res.render('about'))
+server.get('/courses', (req, res) => res.render('courses'))
+
