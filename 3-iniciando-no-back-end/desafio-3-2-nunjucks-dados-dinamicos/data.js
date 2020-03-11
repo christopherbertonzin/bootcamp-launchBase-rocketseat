d7ae66e3f2722669dd94df693c762a548212430a
christopherbertonzin <bertonzin@outlook.com>
2020-03-10 21:22:38 -0300

    desafio-3-2 completo

---
+++ 3-iniciando-no-back-end/desafio-3-2-nunjucks-dados-dinamicos/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        'id': 01,
+        'class':'starter',
+        'img': 'starter.png',
+        'strong': 'Torne-se um programador desejado',
+        'description': 'no mercado com esses cursos gratuitos',
+    },
+    {
+        'id': 02,
+        'class':'lauchBase',
+        'img': 'lauchBase.png',
+        'strong': 'Domine programação do zero',
+        'description': 'e tenha acesso às melhores oportunidades do mercado',
+    },
+    {
+        'id': 01,
+        'class':'goStack',
+        'img': 'goStack.png',
+        'strong': 'Treinamento imersivo',
+        'description': 'nas tecnologias mais modernas de desenvolvimento web e mobile',
+    },
+]
